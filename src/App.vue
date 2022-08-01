353d90ec76061a7bd95f4c14e0d52a8129388e64
whqwq <1776678487@qq.com>
2022-08-01 23:13:27 +0800

    hide 'font-family: Avenir, Helvetica, Arial, sans-serif' from App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 #app {
   height: 0;
   margin: 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
