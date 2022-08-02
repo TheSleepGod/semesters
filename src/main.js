89cdd14a06f6b716d0c488a71ef0f14dc6123f47
shuochen007 <1264133835@qq.com>
2022-08-02 15:26:44 +0800

    lsc 08011526

--- src/main.js
+++ src/main.js
@@ -7,7 +7,17 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
+import vdr from 'vue-draggable-resizable-gorkys'
 
+// 导入默认样式
+import 'vue-draggable-resizable-gorkys/dist/VueDraggableResizable.css'
+import VueDraggableResizable from 'vue-draggable-resizable'
+
+// optionally import default styles
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
+Vue.component('vdr', vdr)
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 new Vue({
