23fef5c1c2a71fa2c6540a3b012252b871b2d352
whqwq <1776678487@qq.com>
2022-08-02 15:28:03 +0800

    whq08021527

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,8 +7,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
+import VueDraggableResizable from 'vue-draggable-resizable'
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
+
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 new Vue({
   router,
   store,
