d99bdc2d7e9b6b46a8bbd8018bdcb1d80ac4befa
MyShirt <20373202@buaa.edu.cn>
2022-08-08 17:38:19 +0800

    gyt08081738

--- src/views/Design2.vue
+++ src/views/Design2.vue
@@ -11,7 +11,7 @@
         <div class="container">
           <div id="bar">
             <h1 style="margin-left: 60px">{{this.name}}</h1>
-
+            <button v-on:click="loadDesign(design)">load Design</button>
             <button v-on:click="saveDesign">Save Design</button>
             <button v-on:click="exportHtml">Export HTML</button>
           </div>
@@ -37,6 +37,7 @@ export default {
   data(){
     return{
       name:'设计图1',
+      design:{},
     }
   },
   methods: {
@@ -49,10 +50,14 @@ export default {
     editorReady() {
       console.log('editorReady');
     },
+    loadDesign(design) {
+      this.$refs.emailEditor.editor.loadDesign(design);
+    },
     saveDesign() {
       this.$refs.emailEditor.editor.saveDesign(
           (design) => {
             console.log('saveDesign', design);
+            this.design=design;
           }
       )
     },
