98fc093acb10f5d91fcf517d729de0bfaa14e53d
TheSleepGod <89347684+TheSleepGod@users.noreply.github.com>
2022-08-08 22:22:35 +0800

    hhw-08082222

--- src/views/DC.vue
+++ src/views/DC.vue
@@ -57,14 +57,15 @@
                 activatable
                 item-key="name"
                 open-on-click
-                style="text-align: left"
+                style="text-align: left; "
+                @contextmenu.prevent="onContextmenu"
             >
-              <template v-slot:prepend="{ item, open }">
-                <v-icon v-if="!item.file">
+              <template v-slot:prepend="{ item, open }" onmousedown="rightC()" >
+                <v-icon v-if="!item.file" style="position: relative; float: left; top: 13px;" @contextmenu.prevent="onContextmenu">
                   {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
                 </v-icon>
-                <v-icon>
-                  {{ files[item.file]}}
+                <v-icon style="position: relative; float: left; top: 13px;" @contextmenu.prevent="onContextmenu">
+                  {{ files[item.file] }}
                 </v-icon>
               </template>
             </v-treeview>
@@ -75,6 +76,10 @@
 </template>
 
 <script>
+
+import Contextmenu from "vue-contextmenujs"
+Vue.use(Contextmenu);
+
 import TopBar from "@/components/topBar";
 import TeamLeft from "@/components/ProjectLeft";
 import TurndownService from 'turndown';
@@ -100,6 +105,7 @@ import docxtemplater from 'docxtemplater'
 import $ from 'jquery'
 import axios from "axios";
 import qs from "qs";
+import Vue from "vue";
 require('@/assets/js/jquery.wordexport')
 
 const getRandomElement = list => {
@@ -246,6 +252,62 @@ export default {
   },
 
   methods: {
+    onContextmenu(event) {
+      this.$contextmenu({
+        items: [
+          {
+            label: "返回(B)",
+            onClick: () => {
+              this.message = "返回(B)";
+              console.log("返回(B)");
+            }
+          },
+          { label: "前进(F)", disabled: true },
+          { label: "重新加载(R)", divided: true, icon: "el-icon-refresh" },
+          { label: "另存为(A)..." },
+          { label: "打印(P)...", icon: "el-icon-printer" },
+          { label: "投射(C)...", divided: true },
+          {
+            label: "使用网页翻译(T)",
+            divided: true,
+            minWidth: 0,
+            children: [{ label: "翻译成简体中文" }, { label: "翻译成繁体中文" }]
+          },
+          {
+            label: "截取网页(R)",
+            minWidth: 0,
+            children: [
+              {
+                label: "截取可视化区域",
+                onClick: () => {
+                  this.message = "截取可视化区域";
+                  console.log("截取可视化区域");
+                }
+              },
+              { label: "截取全屏" }
+            ]
+          },
+          { label: "查看网页源代码(V)", icon: "el-icon-view" },
+          { label: "检查(N)" }
+        ],
+        event, // 鼠标事件信息
+        customClass: "custom-class", // 自定义菜单 class
+        zIndex: 3, // 菜单样式 z-index
+        minWidth: 230 // 主菜单最小宽度
+      });
+      return false;
+    },
+    rightC() {
+      document.oncontextmenu = function(e){
+        return false
+        //或者 e.preventDefault()
+      }
+      document.onmouseup = function(e){
+        if(e.button == 2){
+          console.log('右键点击了');
+        }
+      }
+    },
     setName() {
       const name = (window.prompt('Name') || '')
           .trim()
