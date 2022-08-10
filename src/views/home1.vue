ddfafa9bdd23a75cacf915b83d56c86f155ff9b5
shuochen007 <1264133835@qq.com>
2022-08-10 12:06:37 +0800

    lsc 08101206

--- src/views/home1.vue
+++ src/views/home1.vue
@@ -26,7 +26,7 @@
             <el-input v-model="createUserForm.verifyCode"></el-input>
           </el-form-item>
         </el-form>
-        <div class="primary-btn">立即注册</div>
+        <div class="primary-btn" @click="register()">立即注册</div>
       </div>
     </div>
     <div
@@ -34,9 +34,15 @@
     >
       <form>
         <h2 class="title">登录</h2>
-        <input class="form__input" type="text" placeholder="用户名" v-model="loginForm.username"/>
-        <input class="form__input" type="password" placeholder="密码" v-model="loginForm.password"/>
-        <div class="primary-btn">立即登录</div>
+        <el-form :model="loginForm" :rules="loginRules" label-width="80px" label-position="right">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="loginForm.password" show-password></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="primary-btn" @click="login()">立即登录</div>
       </form>
     </div>
     <div :class="['switch', { login: isLogin }]">
@@ -60,9 +66,40 @@
 </template>
 
 <script>
+import axios from "axios";
+import qs from "qs";
+
 export default {
   name: 'LoginBox',
   data() {
+    let checkUsername = (rule, value, callback) => {
+      let userName = /[_a-zA-Z0-9]{2,12}/;
+      if (userName.test(value)) {	//匹配成功返回
+        callback();
+      }
+      callback(new Error("用户名格式：2-12位英文字母、数字或下划线"));
+    }
+    let checkNewPwd = (rule, value, callback) => {
+      let pwdReg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,18}$/;
+      //如果字符串 string 中含有与 RegExpObject 匹配的文本，则返回 true，否则返回 false。
+      if (!pwdReg.test(value)) {
+        callback(new Error("密码长度8-18位，且需同时包含英文和数字"));
+      }
+      callback();
+    }
+    let checkNewPwd2 = (rule, value, callback) => {
+      if (value !== this.createUserForm.password1) {
+        callback(new Error("两次输入密码不一致！"));
+      }
+      callback();
+    }
+    let checkEmail = (rule, value, callback) => {
+      let mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+      if (mailReg.test(value)) {	//匹配成功返回
+        callback();
+      }
+      callback(new Error("邮箱格式：xx@xx.xx，只含数字、大小写字母、下划线、横杠"));
+    };
     return {
       isLogin: '',
       loginForm: {
@@ -77,12 +114,109 @@ export default {
         email: '',
         verifyCode: '',
       },
+      loginRules: {
+        username: [{required: true, message: '请输入用户名', trigger: 'blur'}, {validator: checkUsername, trigger: 'blur'}],
+        password: [{required: true, message: '请输入密码', trigger: 'blur'}, {validator: checkNewPwd, trigger: 'blur'}],
+      },
+      registerRules: {
+        name: [{required: true, message: '请输入真实姓名', trigger: 'blur'}],
+        username: [{required: true, message: '请输入用户名', trigger: 'blur'}, {validator: checkUsername, trigger: 'blur'}],
+        password1: [{required: true, message: '请输入密码', trigger: 'blur'}, {validator: checkNewPwd, trigger: 'blur'}],
+        password2: [{required: true, message: '请确认密码', trigger: 'blur'}, {validator: checkNewPwd2, trigger: 'blur'}],
+        email: [{required: true, message: '请输入邮箱', trigger: 'blur'}, {validator: checkEmail, trigger: 'blur'}],
+        verifyCode: [{required: true, message: '请输入验证码', trigger: 'blur'}]
+      },
     }
   },
   methods: {
-    login() {},
-    register() {},
+    getNowUser() {
+      this.$axios({
+            method : 'post',
+            url : 'http://101.42.160.94:8000/api/user_web/get_user',
+            headers:{
+              'Authorization':localStorage.getItem('Token'),
+            }
+          }
+      ).then((ret) => {
+        console.log(ret);
+        if (ret.data.errno === 0) {
+          //console.log(ret.data.data);
+          this.$notify.info('已检测到登录信息,即将跳转...')
+          setTimeout(()=>{this.$router.push('/team');},2000)
+        } else this.$notify.error(ret.data.msg);
+        //console.log(this.user_id);
+      })
+    },
+    login: function () {
+      let con = {
+        username:this.loginForm.username,
+        password:this.loginForm.password
+      };
+      //console.log(con);
+      axios({
+        url: 'http://101.42.160.94:8000/api/user_web/login',
+        method: 'post',
+        data: qs.stringify(con),
+      }).then((ret) => {
+        //console.log(ret);
+        if (ret.data.errno === 0) {
+          //console.log(ret);
+          //console.log(ret.data.data.authorization);
+          localStorage.clear();
+          localStorage.setItem('Token',ret.data.data.authorization);
+          this.$message.success("登录成功");
+          console.log("我要跳转啦");
+          this.$router.push('/team');
+        } else {
+          this.$notify.error(ret.data.msg);
+          this.$message.error("登录失败");
+        }
+      })
+    },
+    register: function () {
+      let con = {};
+      con['name'] = this.createUserForm.name;
+      con['password_1'] = this.createUserForm.password1;
+      con['password_2'] = this.createUserForm.password2;
+      con['email'] = this.createUserForm.email;
+      con['code'] = this.createUserForm.verifyCode;
+      con['username'] = this.createUserForm.username;
+      //console.log(con);
+      axios({
+        url: 'http://101.42.160.94:8000/api/user_web/register',
+        method: 'post',
+        data: JSON.stringify(con),
+      }).then((ret) => {
+        if (ret.data.errno === 0) {
+          this.$message.success("注册成功");
+          this.innerVisible = false;
+        } else {
+          alert(ret.data.msg);
+          this.$message.error("登录失败");
+        }
+      })
+    },
+    verify: function () {
+      let con = {};
+      con['email'] = this.createUserForm.email;
+      //console.log(con);
+      axios({
+        url: 'http://101.42.160.94:8000/api/user_web/check_mail',
+        method: 'post',
+        data: JSON.stringify(con),
+      }).then((ret) => {
+        if (ret.data.errno === 0) {
+          this.$message.success("发送成功");
+        } else {
+          this.$notify.error(ret.data.msg);
+          this.$message.error("发送失败");
+        }
+      })
+    },
   },
+  created() {
+    this.getNowUser();
+  }
 }
 </script>
 
